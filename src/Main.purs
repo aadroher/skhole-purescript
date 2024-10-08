44eb8a909eaa18cfeff9376bbf24bc697d75d578
Armand Adroher <aadroher@gmail.com>
2024-10-09 00:15:48 +0200

    Got to use getElementById

--- src/Main.purs
+++ src/Main.purs
@@ -2,24 +2,25 @@ module Main where
 
 import Prelude
 
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(..), maybe)
 import Debug (trace, traceM)
 import Effect (Effect)
 import Effect.Console (log)
 import Web.DOM.Document (createElement)
 import Web.DOM.Element (Element, toNode)
 import Web.DOM.Node (Node, appendChild, nodeName)
+import Web.DOM.NonElementParentNode (getElementById)
 import Web.DOM.ParentNode (ParentNode, QuerySelector(QuerySelector), querySelector)
 import Web.HTML (HTMLDocument, window)
 import Web.HTML.Event.EventTypes (input)
-import Web.HTML.HTMLDocument (toDocument, toParentNode)
+import Web.HTML.HTMLDocument (toDocument, toNonElementParentNode, toParentNode)
 import Web.HTML.Window (document)
 
 appId ∷ String
 appId = "app"
 
-getElementById ∷ String → ParentNode → Effect (Maybe Element)
-getElementById id node = querySelector (QuerySelector $ "#" <> id) node
+getElementById' ∷ String → ParentNode → Effect (Maybe Element)
+getElementById' id node = querySelector (QuerySelector $ "#" <> id) node
 
 
 main :: Effect Unit
@@ -29,9 +30,9 @@ main = do
   traceM w
   d <- document w
   traceM d
-  let n = toParentNode d
-  traceM n
-  maybeAppElement <- getElementById appId n
+  let parentNode = toNonElementParentNode d
+  traceM parentNode
+  maybeAppElement <- getElementById appId parentNode
   case maybeAppElement of
     Just appElement -> do 
       traceM appElement
