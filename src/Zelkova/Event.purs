bf827dfb5e8b18e4311a2aac9399c4b065874fe1
Armand Adroher <aadroher@gmail.com>
2024-10-27 14:32:31 +0100

    WIP

---
+++ src/Zelkova/Event.purs
@@ -0,0 +1,10 @@
+module Zelkova.Event where
+
+import Effect (Effect)
+import Prelude
+
+newtype Event a
+  = Event ((a -> Effect Unit) -> Effect Unit)
+
+instance functorEvent :: Functor Event where
+  map f (Event e) = Event \g -> e (g <<< f)
